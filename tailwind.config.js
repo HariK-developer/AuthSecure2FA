127cc710bdf5a47b6e9837ca0abb6a5186096408
HariK-developer <hariswdeveloper@gmail.com>
2024-07-02 07:38:15 +0530

    login page design

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    
+    extend: {
+      colors: {
+        peach: '#FFE6C9',
+      }
+    },
+  },
+  plugins: [],
+}
